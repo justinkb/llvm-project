3bdd7cbcb22b0dfafd9b5c0a101c880c51b48f36
Luna Nova <git@lunnova.dev>
2024-12-16 14:56:57 -0800

    [AMDGPU] CommonArgs: Enable offload compression by default

--- clang/lib/Driver/ToolChains/CommonArgs.cpp
+++ clang/lib/Driver/ToolChains/CommonArgs.cpp
@@ -3018,7 +3018,7 @@ void tools::addOutlineAtomicsArgs(const Driver &D, const ToolChain &TC,
 void tools::addOffloadCompressArgs(const llvm::opt::ArgList &TCArgs,
                                    llvm::opt::ArgStringList &CmdArgs) {
   if (TCArgs.hasFlag(options::OPT_offload_compress,
-                     options::OPT_no_offload_compress, false))
+                     options::OPT_no_offload_compress, true))
     CmdArgs.push_back("-compress");
   if (TCArgs.hasArg(options::OPT_v))
     CmdArgs.push_back("-verbose");
